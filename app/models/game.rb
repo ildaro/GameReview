0d3de019db88fae6f8b3ad153f45c990ece8996a
unknown <ildar.k@hotmail.co.uk>
2017-10-17 22:01:49 +0100

    scaffolded games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ActiveRecord::Base
+    validates :title, :publisher, :genre, :platform, :description, :image_url, presence: true
+    validates :title, uniqueness: true
+    validates :image_url, format: {
+        with: %r{\.(jpg|png|jpeg)\Z}i,
+        message: 'must be a URL for JPG, JPEG or PNG image.'
+      } 
 end
